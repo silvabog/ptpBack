607acd80d91343d61c476e2c87841ed82c653914
silvabog <silvabog@kean.edu>
2025-04-22 20:10:54 -0400

    db

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
 const { Pool } = require('pg');
 
+// Use hardcoded connection string
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'PassThePages',  // replace with your database name
-    password: '1093759',
-    port: 5432,
+  connectionString: 'postgresql://passthepages_user:I7Xq1n3p9JPhXgdOaCgmnmU9iN8NcXwn@dpg-d0422pbuibrs73amejhg-a.oregon-postgres.render.com/passthepages',
+  ssl: {
+    rejectUnauthorized: false, // required for Render PostgreSQL
+  },
 });
 
 // Test connection
 pool.connect((err, client, release) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-    } else {
-        console.log('Successfully connected to the database');
-    }
-    release();
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+  } else {
+    console.log('Successfully connected to the database');
+  }
+  release();
 });
 
 module.exports = pool;
